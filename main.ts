b1dd4341f8e2cb3a90a7ec4c3ea9f2051cddc64d
OolongJohnson <83778844+OolongJohnson@users.noreply.github.com>
2021-07-23 16:03:06 +0200

    .hex uploaded

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    eletkor += 1
+    basic.showNumber(eletkor)
 })
+input.onButtonPressed(Button.B, function () {
+    eletkor += -1
+    basic.showNumber(eletkor)
+})
+let eletkor = 0
+eletkor = 12
